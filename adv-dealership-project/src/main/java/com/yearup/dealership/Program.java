d5ee41a0be0ec8493ca88d97a8be1c3340ecf8ed
RTech666 <32269553+RTech666@users.noreply.github.com>
2024-04-26 12:51:42 -0400

    Added the files from the previous car dealership project.

---
+++ adv-dealership-project/src/main/java/com/yearup/dealership/Program.java
@@ -0,0 +1,19 @@
+/* 
+Program.java
+This Java file just calls to the UserInterface to start the program.
+*/
+
+package com.yearup.dealership;
+
+public class Program {
+    public static void main(String[] args) {
+        // Create the variable.
+        UserInterface userInterface;
+
+        // Initalize the variable.
+        userInterface = new UserInterface();
+
+        // Call the display method.
+        userInterface.display();
+    }
+}
---
+++ adv-dealership-project/src/main/java/com/yearup/dealership/Vehicle.java
@@ -0,0 +1,67 @@
+/* 
+Vehicle.java
+
+This Java file contains the contructor for the vehicle array and has all the getters for other class files to use to get data as needed.
+*/
+
+package com.yearup.dealership;
+
+class Vehicle {
+    private int vin;
+    private int year;
+    private String make;
+    private String model;
+    private String vehicleType;
+    private String color;
+    private int odometer;
+    private double price;
+
+    public Vehicle(int vin, int year, String make, String model, String vehicleType, String color, int odometer, double price) {
+        this.vin = vin;
+        this.year = year;
+        this.make = make;
+        this.model = model;
+        this.vehicleType = vehicleType;
+        this.color = color;
+        this.odometer = odometer;
+        this.price = price;
+    }
+
+    // Create the getters.
+    public int getVin() {
+        return vin;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getOdometer() {
+        return odometer;
+    }
+
+    public String getVehicleType() {
+        return vehicleType;
+    }
+
+    @Override
+    public String toString() {
+        return "VIN: " + vin + ", Year: " + year + ", Make: " + make + ", Model: " + model + ", Type: " + vehicleType + ", Color: " + color + ", Odometer: " + odometer + ", Price: $" + price;
+    }
+}
